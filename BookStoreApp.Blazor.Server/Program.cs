e649d5502fd235d05ea7ed13ecabfe246bcdc932
Alex Córdova <alexcordovac@outlook.com>
2025-10-31 13:31:53 -0400

    Added User Registration Page

--- BookStoreApp.Blazor.Server/Program.cs
+++ BookStoreApp.Blazor.Server/Program.cs
@@ -1,10 +1,17 @@
 using BookStoreApp.Blazor.Server.Components;
+using BookStoreApp.Blazor.Server.Services.Base;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<IClient, Client >(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiUrl"]);
+
+});
 
 var app = builder.Build();
 
